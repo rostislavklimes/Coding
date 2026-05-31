b562926b624c2d4af8025444b2d6d4df23b7711f
rostislavklimes <rostislavklimes@hotmail.com>
2026-05-31 14:49:42 +0200

    Added circle

---
+++ Python/turtle/circle.py
@@ -0,0 +1,16 @@
+import turtle
+
+zelva = turtle.Turtle("turtle")
+window = turtle.setup(800, 800)
+window = turtle.bgcolor(97/255, 89/255, 247/255)
+
+zelva.penup()
+zelva.right(90)
+zelva.forward(50)
+zelva.left(90)
+zelva.pendown()
+
+
+zelva.circle(100)
+
+turtle.done()
