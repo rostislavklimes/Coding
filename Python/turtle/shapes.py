7fc4afd5b9f22db4816cdd3d78d7f028b2bdf3c8
rostislavklimes <rostislavklimes@hotmail.com>
2026-06-02 21:48:00 +0200

    Added cycle for circle in shapes.py

--- Python/turtle/shapes.py
+++ Python/turtle/shapes.py
@@ -7,7 +7,8 @@ window = turtle.bgcolor(45/255, 237/255, 164/255)
 
 instructions = []
 
-choice = input("Enter square to draw a square, ractangle to draw a ractangle, hexagon to draw a hexagon: ")
+choice = input("Enter square to draw a square, ractangle to draw a ractangle, hexagon to draw a hexagon," \
+              " circle to draw a circle: ")
 
 def shape(shape):
     file = open(f"Python/data/{shape}.trtl", "r")
@@ -21,6 +22,8 @@ elif choice == "ractangle":
     shape("ractangle")
 elif choice == "hexagon":
     shape("hexagon")
+elif choice == "circle":
+    shape("circle")
 
 for i in instructions:
     distance = int((i[1:]))
@@ -31,5 +34,7 @@ for i in instructions:
         zelva.left(distance)
     elif direction == "R":
         zelva.right(distance)
+    elif direction == "C":
+        zelva.circle(distance)
 
 turtle.done()
