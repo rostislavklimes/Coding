4531a7b356b0035c504cd342d77322923e22dae0
rostislavklimes <rostislavklimes@hotmail.com>
2026-06-02 23:20:48 +0200

    Added bank_accont.py to folder clean_python

---
+++ Python/clean_python/bank_account.py
@@ -0,0 +1,55 @@
+balance = 1000
+cash = 500
+transactions = []
+
+def show_balance(balance, cash):
+    print(f"You have {balance}$ on your bank account and {cash}$ in cash")
+    
+
+def transactions_history(transactions):
+    if len(transactions) == 0:
+        print("No transactions yet")
+    else:
+        print("Transactions:")
+        for transaction in transactions:
+            print(f"- {transaction}")
+
+def deposit(amount, balance, cash):
+    if cash >= amount:
+        balance = balance + amount
+        cash = cash - amount
+        transactions.append(f"deposited {amount}$")
+    else:
+        print(f"Not enough money in cash")
+    return balance, cash
+
+def withdraw(amount, balance, cash):
+    if amount <= balance:
+        balance = balance - amount
+        cash = cash + amount
+        transactions.append(f"withdrew {amount}$")
+    else:
+        print("Not enough money on your bank balance")
+    return balance, cash
+
+while True:
+    print()
+    print("1 - show balancene\n2 - show transactions history\n3 - deposit money\n4 - withdraw money\n5 - end")
+    print()
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        print()
+        show_balance(balance, cash)
+    elif choice == 2:
+        print()
+        transactions_history(transactions)
+    elif choice == 3:
+        print()
+        amount = int(input("Enter amount you would like to deposit: "))
+        balance, cash = deposit(amount, balance, cash)
+    elif choice == 4:
+        print()
+        amount = int(input("Enter amount you would like to withdraw: "))
+        balance, cash = withdraw(amount, balance, cash)
+    elif choice == 5:
+        break
